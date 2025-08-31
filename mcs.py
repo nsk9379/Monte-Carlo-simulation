90a07f1739c0451ba2974e7fcdc8c548bc870306
Nishad Kadam <nishadkadam2005@gmail.com>
2025-08-31 20:56:59 +0530

    MCS-4 Implemented Five Heads Simulation

--- mcs.py
+++ mcs.py
@@ -8,3 +8,19 @@ def estimate_yahtzee(num_trials=100000):
         if len(set(dice)) == 1:
             count += 1
     return count / num_trials
+
+
+# (d) Average number of flips before 5 heads in a row
+def estimate_flips_for_5_heads(num_trials=10000):
+    total = 0
+    for _ in range(num_trials):
+        count = heads = 0
+        while heads < 5:
+            flip = random.choice(['H','T'])
+            count += 1
+            if flip == 'H':
+                heads += 1
+            else:
+                heads = 0
+        total += count
+    return total / num_trials
