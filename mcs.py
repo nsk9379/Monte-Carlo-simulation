f59adb21a5a99297202b838ccc6fd27c94d63651
Nishad Kadam <nishadkadam2005@gmail.com>
2025-08-31 21:03:53 +0530

    MCS-3 Implemented Longest Run Estimation

--- mcs.py
+++ mcs.py
@@ -19,6 +19,24 @@ def estimate_large_straight(num_trials=100000):
             count += 1
     return count / num_trials
 
+# (c) Average longest run of heads/tails in 200 flips
+def estimate_longest_run(num_trials=10000, flips=200):
+    def longest_run(seq):
+        max_run = run = 1
+        for i in range(1, len(seq)):
+            if seq[i] == seq[i-1]:
+                run += 1
+                max_run = max(max_run, run)
+            else:
+                run = 1
+        return max_run
+    
+    total = 0
+    for _ in range(num_trials):
+        sequence = [random.choice(['H','T']) for _ in range(flips)]
+        total += longest_run(sequence)
+    return total / num_trials
+
 # (d) Average number of flips before 5 heads in a row
 def estimate_flips_for_5_heads(num_trials=10000):
     total = 0
