112440f8c81c9ab583f649da6f90b29fd944926c
Nishad Kadam <nishadkadam2005@gmail.com>
2025-08-31 21:02:30 +0530

    MCS-2 Implemented Large Straight Probability Simulation

--- mcs.py
+++ mcs.py
@@ -9,6 +9,15 @@ def estimate_yahtzee(num_trials=100000):
             count += 1
     return count / num_trials
 
+# (b) Probability of rolling a large straight (1-2-3-4-5 or 2-3-4-5-6)
+def estimate_large_straight(num_trials=100000):
+    straights = [{1,2,3,4,5}, {2,3,4,5,6}]
+    count = 0
+    for _ in range(num_trials):
+        dice = [random.randint(1, 6) for _ in range(5)]
+        if set(dice) in straights:
+            count += 1
+    return count / num_trials
 
 # (d) Average number of flips before 5 heads in a row
 def estimate_flips_for_5_heads(num_trials=10000):
